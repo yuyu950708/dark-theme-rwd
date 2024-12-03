706258d700b3842c348c4051514f445ad7954c8e
yuyu950708 <jieyu950708@gmail.com>
2024-12-03 16:46:10 +0800

    init

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,16 +2,25 @@ import * as React from 'react'
 import Stack from '@mui/material/Stack'
 import Button from '@mui/material/Button'
 import { IoAmericanFootballSharp } from 'react-icons/io5'
+import style from "@/styles/Home.module.scss"
 
 export default function Home() {
   return (
     <>
-      <IoAmericanFootballSharp />
-
+      <div className={style.abc}>Lorem ipsum dolor sit amet.</div>
       <Stack spacing={2} direction="row">
         <Button variant="text">Text</Button>
         <Button variant="contained">Contained</Button>
-        <Button variant="outlined">Outlined</Button>
+
+        <Button
+          component="label"
+          role={undefined}
+          variant="contained"
+          tabIndex={-1}
+          startIcon={<IoAmericanFootballSharp />}
+        >
+          Upload files
+        </Button>
       </Stack>
     </>
   )
